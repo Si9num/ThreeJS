dd318986a650d10ed2d67dea2f17c0bc903ce20f
Kiryl Yastremski <78493168+Si9num@users.noreply.github.com>
2022-08-02 02:45:24 +0300

    feat: add movement

--- firstTask/index.js
+++ firstTask/index.js
@@ -1,27 +1,28 @@
 import "./index.css";
-import { l, animate } from "./cube";
-class Control {
-  constructor(name) {
-    this.name = name;
-  }
+import { l, animate, cube, renderer, scene, camera } from "./cube";
 
-  run() {
-    document.querySelector(`.runButton`).addEventListener("click", () => {
-      let start = document.querySelector(".from").value;
-      let finsish = document.querySelector(".to").value;
-      document.querySelector(
-        `${this.name}`
-      ).style.transform = ` matrix(1, 0, 0, 1, ${start}, 0)`;
-      document.querySelector(`${this.name}`).style.transition = `${
-        document.querySelector(".speed").value
-      }s`;
-      document.querySelector(
-        `${this.name}`
-      ).style.transform = ` matrix(1, 0, 0, 1, ${finsish}, 0)`;
-    });
-  }
+function run() {
+  document.querySelector(`.runButton`).addEventListener("click", () => {
+    let start = document.querySelector(".from").value;
+    let finish = document.querySelector(".to").value;
+    let speed = document.querySelector(".speed").value;
+    cube.position.x = +start;
+    let req = 0;
+    (function move() {
+      req = requestAnimationFrame(move);
+
+      cube.position.x += +speed;
+      if (+cube.position.x > +finish) {
+        cancelAnimationFrame(req);
+        req = 0;
+      }
+
+      renderer.render(scene, camera);
+      console.log(cube.position.x);
+    })();
+  });
 }
-let check = new Control(".block");
-check.run();
+
+run();
 l();
 animate();
